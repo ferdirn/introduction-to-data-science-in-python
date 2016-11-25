44ad8dd8f1dfadc96b685b317722639e1e0b6616
Ferdi Ramdhon <ferdirn@gmail.com>
2016-11-25 10:21:40 +0700

    average city fuel economy for each cylinder group

---
+++ avg_city_fuel_economy_each_cylinder_group.py
@@ -0,0 +1,13 @@
+CtyMpgByCyl = []
+
+for c in cylinders: # iterate over all the cylinder levels
+    summpg = 0
+    cyltypecount = 0
+    for d in mpg: # iterate over all dictionaries
+        if d['cyl'] == c: # if the cylinder level type matches,
+            summpg += float(d['cty']) # add the cty mpg
+            cyltypecount += 1 # increment the count
+    CtyMpgByCyl.append((c, summpg / cyltypecount)) # append the tuple ('cylinder', 'avg mpg')
+
+CtyMpgByCyl.sort(key=lambda x: x[0])
+CtyMpgByCyl
